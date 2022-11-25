c2a28ca9635eebfa73b3ca6da8aa07c3ca46ff01
letienloc <letienloc2001@gmail.com>
2022-11-25 23:31:01 +0700

    init commit

--- cfg.py
+++ cfg.py
@@ -1,22 +1,22 @@
 class Config:
     # paths for synthetic train dataset
-    train_x_syn = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/img/*.png'
-    train_y_syn = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/label/*.png'
-    train_mask_syn = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/train/all_gts/*.txt'
+    train_x = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/img/*.png'
+    train_y = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/label/*.png'
+    train_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/bbox/*.txt'
     # paths for synthetic test Dataset
-    test_x_syn = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/img/*.png'
-    test_y_syn = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/label/*.png'
-    test_mask_syn = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/test/all_gts/*.txt'
+    test_x = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/img/*.png'
+    test_y = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/label/*.png'
+    test_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/test/all_gts/*.txt'
 
-    # paths for SCUT-real train Dataset
-    train_x = 'Data/real/train/all_images/*.jpg'
-    train_y = 'Data/real/train/all_labels/*.jpg'
-    train_mask = 'Data/real/train/all_gts/*.txt'
+    # # paths for SCUT-real train Dataset
+    # train_x = 'Data/real/train/all_images/*.jpg'
+    # train_y = 'Data/real/train/all_labels/*.jpg'
+    # train_mask = 'Data/real/train/all_gts/*.txt'
 
-    # paths for SCUT-real test Dataset
-    test_x = 'Data/real/test/all_images/*.jpg'
-    test_y = 'Data/real/test/all_labels/*.jpg'
-    test_mask = 'Data/real/test/all_gts/*.txt'
+    # # paths for SCUT-real test Dataset
+    # test_x = 'Data/real/test/all_images/*.jpg'
+    # test_y = 'Data/real/test/all_labels/*.jpg'
+    # test_mask = 'Data/real/test/all_gts/*.txt'
 
 
     # train conig
