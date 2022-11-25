b91541a09f6a3986296e59b8061a6476ef3e4faa
letienloc <letienloc2001@gmail.com>
2022-11-25 23:31:15 +0700

    init commit

--- cfg.py
+++ cfg.py
@@ -6,7 +6,7 @@ class Config:
     # paths for synthetic test Dataset
     test_x = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/img/*.png'
     test_y = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/label/*.png'
-    test_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/test/all_gts/*.txt'
+    test_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/bbox/*.txt'
 
     # # paths for SCUT-real train Dataset
     # train_x = 'Data/real/train/all_images/*.jpg'
