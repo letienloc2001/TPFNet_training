ad35faccac1326c37d706c1009dfe4dfe2b449aa
letienloc <letienloc2001@gmail.com>
2022-11-27 00:57:31 +0700

    init commit

--- cfg.py
+++ cfg.py
@@ -23,7 +23,7 @@ class Config:
     batch_size = 32
     epochs=400
     saved_model_path = '/content/drive/Shareddrives/HCMUT/DACN/TPF_saved_model/'
-    num_worker=3
+    num_worker=1
     lr=1e-4
 
     
