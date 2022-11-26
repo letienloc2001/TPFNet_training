11172398ac844b136ee8bfbd2e1d5a596daf46b3
letienloc <letienloc2001@gmail.com>
2022-11-26 09:18:47 +0700

    init commit

--- cfg.py
+++ cfg.py
@@ -1,12 +1,12 @@
 class Config:
     # paths for synthetic train dataset
-    train_x = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/img/*.png'
-    train_y = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/label/*.png'
-    train_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_train/bbox/*.txt'
+    train_x = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-1K/syn_train/img/*.png'
+    train_y = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-1K/syn_train/label/*.png'
+    train_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-1K/syn_train/bbox/*.txt'
     # paths for synthetic test Dataset
-    test_x = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/img/*.png'
-    test_y = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/label/*.png'
-    test_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-8K/syn_test/bbox/*.txt'
+    test_x = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-1K/syn_test/img/*.png'
+    test_y = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-1K/syn_test/label/*.png'
+    test_mask = '/content/drive/Shareddrives/HCMUT/DACN/data_collection/SCUT-1K/syn_test/bbox/*.txt'
 
     # # paths for SCUT-real train Dataset
     # train_x = 'Data/real/train/all_images/*.jpg'
